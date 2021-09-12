46a7d4a0714bf625832b8349ae807ba086e5c3f0
Roshan Rasal <43706726+Rosh3639@users.noreply.github.com>
2021-09-12 14:28:40 +0530

    Update main_helper.py

--- main_helper.py
+++ main_helper.py
@@ -2,7 +2,7 @@ import pyowm
 from datetime import datetime
 from api import API_KEY
 
-# Add your own API_KEY from OpenWeather.com
+# Add your own API_KEY from OpenWeatherMap.com
 owm = pyowm.OWM(API_KEY)
 mgr = owm.weather_manager()
 forecaster = mgr.forecast_at_place('Nashik, India', '3h')
