bc143034ab7b841888fdcd98b0e18439ddee44a1
Roshan <rshnrasal@gmail.com>
2021-09-12 14:17:04 +0530

    Python based weather GUI

---
+++ api.py
@@ -0,0 +1,4 @@
+
+
+# Weather API key
+API_KEY = "1eb8c60080f8d1f08a5c542b26a7b9be"
---
+++ main_timezone_conversion.py
@@ -0,0 +1,11 @@
+import pytz
+from datetime import datetime
+
+
+def gmt_to_eastern(unix_gmt):
+    pass
+
+
+if __name__ == '__main__':
+    for timezone in pytz.all_timezones:
+        print(timezone)
